8ce9880042c7970ac28456a8e3da6918a8dae94d
Maggie Jane <151887309+MaggieJane95@users.noreply.github.com>
2024-06-04 14:12:50 -0400

    first attempt at loading the map

---
+++ leaflet starter code/static/js/logic.js
@@ -0,0 +1,74 @@
+// Define our URL 
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson";
+
+// creating the map
+let myMap = L.map("map", {
+    center: [51.505, -0.09],
+    zoom: 5
+
+});
+
+// Creating the background or Title Layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Fetch the earthquake data
+d3.json(url).then(function(data) {
+    // Function to determine marker size based on magnitude
+    function markerSize(magnitude) {
+        return magnitude * 4;
+    }
+    // Function to determine marker color based on depth
+    function markerColor(depth) {
+        return depth > 90 ? '#d73027' :
+               depth > 70 ? '#fc8d59' :
+               depth > 50 ? '#fee08b' :
+               depth > 30 ? '#d9ef8b' :
+               depth > 10 ? '#91cf60' :
+                            '#1a9850';
+    }
+    // Function to create markers and bind popups
+    function createMarkers(feature, latlng) {
+        let options = {
+            radius: markerSize(feature.properties.mag),
+            fillColor: markerColor(feature.geometry.coordinates[2]),
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+        
+        let marker = L.circleMarker(latlng, options);
+
+        marker.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+
+        return marker;
+    }
+
+    // Create a GeoJSON layer with the data
+    L.geoJSON(data, {
+        pointToLayer: createMarkers
+    }).addTo(myMap);
+
+    // Create a legend
+    let legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function(map) {
+        let div = L.DomUtil.create('div', 'info legend'),
+            depths = [-10, 10, 30, 50, 70, 90],
+            labels = [];
+
+        div.innerHTML += '<h4>Depth</h4>'
+
+        for (let i = 0; i < depths.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + markerColor(depths[i] + 1) + '"></i> ' +
+                depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
+});
